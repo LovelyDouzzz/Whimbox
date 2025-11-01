43029dd76dafe146d677d731bef5759f57a23743
userkey <5314175+userkey@user.noreply.gitee.com>
2025-11-01 20:22:22 +0800

    延长拍照后的检测时间，避免低性能电脑卡太久

--- whimbox/task/photo_task/daily_photo_task.py
+++ whimbox/task/photo_task/daily_photo_task.py
@@ -18,7 +18,7 @@ class DailyPhotoTask(TaskTemplate):
 
     @register_step("删除任务照片")
     def step3(self):
-        if wait_until_appear(IconPhotoEdit):
+        if wait_until_appear(IconPhotoEdit, retry_time=10):
             self.update_task_result(message="每日任务拍照完成")
             itt.move_and_click(IconPhotoDelete.cap_center_position_xy)
             if wait_until_appear_then_click(ButtonPhotoDeleteConfirm):
@@ -31,6 +31,7 @@ class DailyPhotoTask(TaskTemplate):
         ui_control.goto_page(page_main)
 
 if __name__ == "__main__":
-    daily_photo_task = DailyPhotoTask()
-    daily_photo_task.task_run()
-    print(daily_photo_task.task_result)
+    # daily_photo_task = DailyPhotoTask()
+    # daily_photo_task.task_run()
+    # print(daily_photo_task.task_result)
+    ui_control.goto_page(page_photo)
